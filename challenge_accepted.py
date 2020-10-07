f449f444fb1a9ccd1723b6e3be6a7aa3f28c37db
Sinister_30N4 <36511097+tongJK@users.noreply.github.com>
2020-10-07 15:08:54 +0700

    Update challenge_accepted.py

--- challenge_accepted.py
+++ challenge_accepted.py
@@ -23,7 +23,8 @@ def string2int(sentence_input):
         for digit in digits:
             if alphabet in digit:
                 result = result + alphabet
-    return int(result) 
+
+    return int(result) if result else None
 
 
 def recheck(sentence_input):
@@ -31,14 +32,27 @@ def recheck(sentence_input):
     This function to recheck that my function(string2int) work well
     """
 
-    return ''.join(filter(lambda i: i.isdigit(), sentence_input))
+    return ''.join(filter(lambda i: i.isdigit(), sentence_input)) 
 
 
 if __name__ == '__main__':
 
-    test_list = ['abc573', 'a5b7c3', 'fsipfjqw0-efjqw0-กไนดไยบำดนาไำขดนาไขดนาไำ123dfwmpefmpหดายนำยไดข-/ตคภ/ตค/--++   ']
+    #
+    # ---------- use this block for set value's input ------------------------------------------------------------
+    
+    # test_list = ['abc573', 'a5b7c3', 'fsipfjqw0-efjqw0-mpefmpหดาย-/ตคภ/ตค/--++ ', 'asdasd', '123456789']
+    # 
+    # for i, test in enumerate(test_list):
+    #     print('test {} : input is "{}", my funtion\'s result is {} and recheck result is {}'
+    #           .format((i+1), test, string2int(test), recheck(test)))
+    # ------------------------------------------------------------------------------------------------------------
+    
+    #
+    # ---------- use this block for set keyboard's input ---------------------------------------------------------
+    while True:
+        given_input = input('Enter your input: ')
+        int_result = string2int(given_input)
+        print('given input is "{}", result is {}, type of result is {}\n'.format(
+            given_input, int_result, type(int_result)))
 
-    for i, test in enumerate(test_list):
-        print('test {} : input is "{}", my funtion\'s result is "{}" and recheck result is "{}"'
-              .format((i+1), test, string2int(test), recheck(test)))
-
+    # ------------------------------------------------------------------------------------------------------------
