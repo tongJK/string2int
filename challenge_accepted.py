a663d1beb8d4e2e035e87499641e8c53c4a95318
Sinister_30N4 <36511097+tongJK@users.noreply.github.com>
2020-11-10 21:05:14 +0700

    Update challenge_accepted.py

--- challenge_accepted.py
+++ challenge_accepted.py
@@ -1,5 +1,5 @@
 """
-Challenge Accepted.
+Challenge Accepted
 
 By : Jakgri Klabdi (https://github.com/tongJK)
 
@@ -7,50 +7,23 @@ UUsing : Python 3.6.6
 
 Started : 30/9/2020 20.50
 
-Finished : 30/9/2020 21.10
-
-Edited : 07/10/2020 19.25
+Finished : 10/11/2020 21.10
 """
-
 
 digits = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 
 
-def string2int(sentence_input):
-    """
-    This is my function to extract numerical from any input sentence
-    """
-    result = None
-    for alphabet in sentence_input:
-        if alphabet in digits:
-            if result:
-                result = (result*10) + digits[alphabet]
-            else:
-                result = digits[alphabet]
-
-    return result
-
-
 if __name__ == '__main__':
 
-    # #
-    # # ---------- use this block for set value's input ------------------------------------------------------------
-    #
-    # test_list = ['abc573', 'a5b7c3', 'fsipfjqw0-efjqw0-mpefmpหดาย-/ตคภ/ตค/--++ ', 'asdasd', '123456789']
-    #
-    # for i, test in enumerate(test_list):
-    #     int_result = string2int(test)
-    #     print('test {} : input is "{}", my funtion\'s result is "{}", type of result is {}'.format(
-    #         (i+1), test, int_result, type(int_result)))
-    # # ------------------------------------------------------------------------------------------------------------
-
-    #
-    # ---------- use this block for set keyboard's input ---------------------------------------------------------
-
     while True:
+        int_result = None
         given_input = input('Enter your input: ')
-        int_result = string2int(given_input)
-        print('given input is "{}", result is {}, type of result is {}\n'.format(
-            given_input, int_result, type(int_result)))
 
-    # ------------------------------------------------------------------------------------------------------------
+        for alphabet in given_input:
+            if alphabet in digits:
+                if int_result:
+                    int_result = (int_result*10) + digits[alphabet]
+                else:
+                    int_result = digits[alphabet]
+
+        print('given input is "{}", result is {}\n'.format(given_input, int_result))
